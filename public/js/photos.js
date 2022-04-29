0d348ebaa1feeae168835c706a1aaf978ba9b107
vasily <moonalty@gmail.com>
2022-04-29 10:37:40 +0300

    photoAlbum release 3

--- public/js/photos.js
+++ public/js/photos.js
@@ -7,18 +7,23 @@ colomnDiv.addEventListener("click", async (event) => {
   event.preventDefault();
   // console.log(event.target);
   const tick = event.target.id;
+
   // console.log("ID>>>>", tick);
-  const response = await fetch("/allAlbums", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: tick,
-    }),
-  });
-  const data = await response.text();
-  photos.innerHTML = data;
+  if (tick === "colomnDiv") {
+    console.log(")))))))))");
+  } else {
+    const response = await fetch("/allAlbums", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: tick,
+      }),
+    });
+    const data = await response.text();
+    photos.innerHTML = data;
+  }
   // console.log(data);
 });
 
